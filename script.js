fbae003a61b053df8f03da3a682dacdfcdd28bf2
Lara Reyes <126874982+Lara-Reyes@users.noreply.github.com>
2023-06-27 19:59:18 -0300

    delete btn and making it more dynamic

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  renderLeads();
-});
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-function renderLeads() {
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-        <a target='_blank' href='${myLeads[i]}'>
-             ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'>
+             ${leads[i]}
         </a>
     </li>
     `;
   }
-
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
